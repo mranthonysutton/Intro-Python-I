cc2f853ab10ea8853ee3b4b8aaa6ec5bb5b28411
Anthony Sutton <anthony.sutton95@gmail.com>
2020-03-09 13:37:05 -0700

    Completed most of the problems for args)

--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,9 @@
 
 # YOUR CODE HERE
 
+def f1(num1, num2):
+    return num1 + num2
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -14,6 +17,10 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+def f2(*args):
+    num = [x for x in args]
+    return sum(num)
+
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -21,8 +28,8 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+# How do you have to modify the f2 call below to make this work? I had to add "*" in front of the a
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +37,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(a, b = 1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +53,8 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+
+# TODO: Make def f4 function
 
 # Should print
 # key: a, value: 12
